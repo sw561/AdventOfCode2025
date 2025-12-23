6efac19b12bd585707d033478d99feed42459372
Simon Wilkinson <simon_wilkinson92@hotmail.com>
2025-12-23 15:17:38 +0000

    Day 4 part 2

--- day4/day4.py
+++ day4/day4.py
@@ -4,15 +4,15 @@ import fileinput
 
 def padding(s):
     ncols = len(s[0])
-    padded_row = "." * (ncols + 2)
+    padded_row = ["."] * (ncols + 2)
     ret = [padded_row]
     for row in s:
-        ret.append("." + row + ".")
+        ret.append(["."] + row + ["."])
     ret.append(padded_row)
     return ret
 
-def count_accessible(s):
-    accessible = 0
+def accessible(s):
+    accessible = []
     for i in range(len(s)):
         for j in range(len(s[i])):
             if s[i][j]!='@': continue
@@ -22,13 +22,32 @@ def count_accessible(s):
                 for jj in range(j-1,j+2):
                     count += 1 if s[ii][jj] == '@' else 0
 
-            accessible += 1 if count <= 4 else 0
+            if count <= 4:
+                accessible.append((i, j))
 
     return accessible
 
+def part2(s):
+    total_removed = 0
+    while True:
+        a = accessible(s)
+        if len(a) == 0: break
+
+        for i, j in a:
+            s[i][j] = '.'
+            total_removed += 1
+
+    return total_removed
+
+
+def count_accessible(s):
+    return len(accessible(s))
+
 if __name__=="__main__":
-    s = padding([line.strip() for line in fileinput.input()])
+    s = padding([list(line.strip()) for line in fileinput.input()])
 
     part1 = count_accessible(s)
+    part2 = part2(s)
 
     print(part1)
+    print(part2)
--- day4/test_day4.py
+++ day4/test_day4.py
@@ -1,6 +1,6 @@
 import pytest
 
-from day4 import padding, count_accessible
+from day4 import padding, count_accessible, part2
 
 def test_example():
     s = ["..@@.@@@@.",
@@ -15,5 +15,6 @@ def test_example():
          "@.@.@@@.@.",
          ]
 
-    s = padding(s)
+    s = padding([list(i) for i in s])
     assert count_accessible(s) == 13
+    assert part2(s) == 43
