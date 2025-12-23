143ffa486639a54df37486ce1b28a26251f47bee
Simon Wilkinson <simon_wilkinson92@hotmail.com>
2025-12-23 14:49:50 +0000

    Day 3 part 2

--- day3/day3.py
+++ day3/day3.py
@@ -2,17 +2,34 @@
 
 import fileinput
 
-def max_joltage(s_orig):
-    s = [int(x) for x in s_orig]
+def int_builder(g):
+    x = 0
+    for d in g:
+        x = x * 10 + d
+    return x
+
+def max_joltage(*args, **kwargs):
+    return int_builder(max_joltage_(*args, **kwargs))
+
+def max_joltage_(s, n=2, start=0):
+    if n == 1:
+        yield max(s[start:])
+        return
 
     # Prioritise early occurences
-    first = max((x,-i) for i,x in enumerate(s[:-1]))
-    first_index = -first[1]
-    second = max(s[first_index+1:])
+    choice = max((s[i],-i) for i in range(start, len(s)-(n-1)))
+    choice_index = -choice[1]
 
-    ret = first[0]*10 + second
-    return ret
+    yield choice[0]
+    yield from max_joltage_(s, n-1, choice_index+1)
 
 if __name__=="__main__":
-    s = sum(max_joltage(line.strip()) for line in fileinput.input())
-    print(s)
+    part1 = 0
+    part2 = 0
+    for line in fileinput.input():
+        s = [int(x) for x in line.strip()]
+        part1 += max_joltage(s)
+        part2 += max_joltage(s, n=12)
+
+    print(part1)
+    print(part2)
--- day3/test_day3.py
+++ day3/test_day3.py
@@ -1,11 +1,13 @@
 import pytest
 from day3 import max_joltage
 
-@pytest.mark.parametrize("s, expected", 
-    [("987654321111111", 98),
-     ("811111111111119", 89),
-     ("234234234234278", 78),
-     ("818181911112111", 92),
+@pytest.mark.parametrize("s, p1, p2", 
+    [("987654321111111", 98, 987654321111),
+     ("811111111111119", 89, 811111111119),
+     ("234234234234278", 78, 434234234278),
+     ("818181911112111", 92, 888911112111),
     ])
-def test_invalids(s, expected):
-    assert max_joltage(s) == expected
+def test_joltage(s, p1, p2):
+    s = [int(x) for x in s]
+    assert max_joltage(s) == p1
+    assert max_joltage(s, n=12) == p2
