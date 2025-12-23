a49a34f88de61f856a2398f4a0c5b8130c00e3b9
Simon Wilkinson <simon_wilkinson92@hotmail.com>
2025-12-23 16:43:16 +0000

    Day 6 part 2

--- day6/day6.py
+++ day6/day6.py
@@ -11,17 +11,48 @@ def multiply(numbers):
 d = {'*': multiply, '+': sum}
 
 def part1(data, ops):
+    ops = ops.split()
+    data = [list(map(int, row.split())) for row in data]
+
     total = 0
     for i in range(len(data[0])):
-        total += d[last[i]](row[i] for row in data)
+        total += d[ops[i]](row[i] for row in data)
+    return total
+
+def get_edges(ops):
+    for i in range(len(ops)):
+        if ops[i] != ' ':
+            yield i
+    yield len(ops)+1
+
+def drop_one(g):
+    next(g)
+    yield from g
+
+def build_int(digits):
+    ret = 0
+    for d in digits:
+        if d == ' ': continue
+        ret = ret * 10 + int(d)
+    return ret
+
+def part2(data, ops):
+    total = 0
+    for start, end in zip(get_edges(ops), drop_one(get_edges(ops))):
+        total += d[ops[start]](
+            (build_int(row[index] for row in data))
+                for index in range(start, end-1)
+            )
+
     return total
 
 if __name__=="__main__":
     
-    data = [line.strip().split() for line in fileinput.input()]
-
-    ops = data[-1]
-    data = [list(map(int, row)) for row in data[:-1]]
+    data = [line.strip('\n') for line in fileinput.input()]
     
+    ops = data[-1]
+    data = data[:-1]
+
     print(part1(data, ops))
+    print(part2(data, ops))
 
