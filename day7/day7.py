09346095f3da71f5b5096978cd7d6002066298b3
Simon Wilkinson <simon_wilkinson92@hotmail.com>
2025-12-24 19:22:08 +0000

    Day 7 part 2

--- day7/day7.py
+++ day7/day7.py
@@ -1,31 +1,32 @@
 #!/usr/bin/env python
 
 import fileinput
+from collections import defaultdict
 
 def simulate(data):
     splits = 0
 
-    beams = set()
+    beams = dict() # for each position, beams[position] is number of worlds
 
     for i, x in enumerate(data[0]):
         if x == "S":
-            beams.add(i)
+            beams[i] = 1
             break
 
     for row in data[1:]:
 
-        new_beams = set()
-        for b in beams:
+        new_beams = defaultdict(lambda: 0)
+        for b, n in beams.items():
             if row[b] == '.':
-                new_beams.add(b)
+                new_beams[b] += n
             elif row[b] == '^':
-                new_beams.add(b-1)
-                new_beams.add(b+1)
+                new_beams[b-1] += n
+                new_beams[b+1] += n
                 splits += 1
 
         beams = new_beams
 
-    return splits
+    return splits, sum(beams.values())
 
 if __name__=="__main__":
     data = [line.strip() for line in fileinput.input()]
