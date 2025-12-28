5f6fa04578d64485f49fa7583ccb2254d4586919
Simon Wilkinson <simon_wilkinson92@hotmail.com>
2025-12-28 13:09:18 +0000

    Day 8 much faster

--- day8/day8.py
+++ day8/day8.py
@@ -2,49 +2,12 @@
 
 import fileinput
 from itertools import combinations
-from collections import defaultdict
+from collections import Counter
 
 def distance2(c1, c2):
     return sum((c1i-c2i)**2 for c1i, c2i in zip(c1, c2))
 
-def build_groups(n, edges, edge_n, part1=False):
-
-    # Build the groups.
-    # If part1 get product of three largest group sizes
-    # If part2 just return boolean:
-    #   True : only one large group,
-    #   False: more than one group
-
-    # Now build groups using the edges
-    groups = []
-    visited = set()
-    for i in range(n):
-        if i in visited: continue
-
-        group_size = 1
-        visited.add(i)
-        neighbours = [i]
-
-        while neighbours:
-            neighbour = neighbours.pop()
-            for (edge_i, j) in edges[neighbour]:
-                if j in visited: continue
-                if edge_i >= edge_n: continue
-
-                group_size += 1
-                visited.add(j)
-                neighbours.append(j)
-
-        if not part1:
-            return group_size == n
-        groups.append(group_size)
-
-    groups.sort(reverse=True)
-
-    part1 = groups[0] * groups[1] * groups[2]
-    return part1
-
-def solve(coords, n_edges=10):
+def solve(coords, n_edges):
     n = len(coords)
 
     es = sorted(
@@ -52,39 +15,39 @@ def solve(coords, n_edges=10):
         key=lambda c: distance2(coords[c[0]], coords[c[1]])
         )
 
-    edges = defaultdict(list)
-    for ei, (i, j) in enumerate(es):
-        edges[i].append((ei, j))
-        edges[j].append((ei, i))
+    # group_id[i] = g means that node i is in group g
+    group_id = [i for i in range(n)]
+    count_group0 = 1
 
-    # edges[i] includes (ei, j) means i and j are connected iff number of edges > ei
+    for ei, (i, j) in enumerate(es, start=1):
+        # print("Connecting", coords[i], coords[j])
+        if group_id[i] != group_id[j]:
+            assign_group = group_id[i]
+            connect_group = group_id[j]
 
-    # Now build groups using the edges
-    part1 = build_groups(n, edges, n_edges, part1=True)
+            if assign_group > connect_group:
+                assign_group, connect_group = connect_group, assign_group
 
-    def objective(m):
-        return build_groups(n, edges, m)
+            count_connections = 0
+            for n_i in range(n):
+                if group_id[n_i] == connect_group:
+                    group_id[n_i] = assign_group
+                    count_connections += 1
 
-    # Expect one big group for right, not left
-    # i.e. build_groups returns True for right not left
-    left = 0
-    right = len(es)
+            if assign_group == 0:
+                count_group0 += count_connections
 
-    assert objective(right) == True
-    assert objective(left) == False
+            if count_group0 == n:
+                part2 = coords[i][0] * coords[j][0]
+                break
 
-    # for m in range(0, right):
-    #     print(m, objective(m), coords[es[m][0]], coords[es[m][1]])
-    while right - left > 1:
-        m = (left + right) // 2
-        if objective(m):
-            right = m
-        else:
-            left = m
+        if ei == n_edges:
+            # Do part 1 here
 
-    i, j = es[right-1]
-    # print(coords[i], coords[j])
-    part2 = coords[i][0] * coords[j][0]
+            large_groups = Counter(group_id).most_common(3)
+            part1 = 1
+            for lg in large_groups:
+                part1 *= lg[1]
 
     return part1, part2
 
